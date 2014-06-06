bd5856ac32ff83b21d5fcc7cc0eb9a57031f700b
Viktor Farcic <viktor.farcic@everis.com>
2014-06-06 15:23:04 +0200

    Something

--- app/controllers/BooksController.scala
+++ app/controllers/BooksController.scala
@@ -1,12 +1,31 @@
 package controllers
 
-import play.api.mvc.{Action, Controller}
+import play.api.mvc.{BodyParsers, Action, Controller}
 import play.api.libs.json.{JsPath, Writes}
 import models.Book
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
 
 object BooksController extends Controller {
+
+  def listAll = Action {
+    Ok(Json.toJson(Book.list))
+  }
+
+  def save = Action(BodyParsers.parse.json) { request =>
+    val bookResult = request.body.validate[Book]
+    bookResult.fold(
+      errors => {
+        BadRequest(Json.obj("status" -> "KO", "message" -> JsError.toFlatJson(errors)))
+      },
+      book => {
+        Book.save(book)
+        val title = book.title
+        Ok(Json.obj("status" -> "OK", "message" -> s"Book $title has been saved."))
+      }
+    )
+  }
+
 
   implicit val bookWrites: Writes[Book] = (
     (JsPath \ "id").write[Int] and
@@ -17,8 +36,13 @@ object BooksController extends Controller {
     (JsPath \ "link").write[String]
   )(unlift(Book.unapply))
 
-  def listAll = Action {
-    Ok(Json.toJson(Book.list))
-  }
+  implicit val bookReads: Reads[Book] = (
+    (JsPath \ "id").read[Int] and
+    (JsPath \ "image").read[String] and
+    (JsPath \ "title").read[String] and
+    (JsPath \ "author").read[String] and
+    (JsPath \ "price").read[Double] and
+    (JsPath \ "link").read[String]
+  )(Book.apply _)
 
 }
