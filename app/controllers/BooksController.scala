0f3f60aa7278cf0124cd82dfdc0c50f5f5a98464
Viktor <trustno1>
2014-06-10 09:43:59 +0200

    Added initial set of front-end files

--- app/controllers/BooksController.scala
+++ app/controllers/BooksController.scala
@@ -1,12 +1,14 @@
 package controllers
 
-import play.api.mvc.{BodyParsers, Action, Controller}
-import play.api.libs.json.{JsPath, Writes}
-import models.Book
+import play.api.mvc.{AnyContent, BodyParsers, Action, Controller}
+import models.{BookSerializer, Book}
 import play.api.libs.json._
-import play.api.libs.functional.syntax._
 
-object BooksController extends Controller {
+object BooksController extends Controller with BookSerializer {
+
+  def index: Action[AnyContent] = Action {
+    Ok(scala.io.Source.fromFile("public/html/index.html").mkString).as("text/html")
+  }
 
   def listAll = Action {
     Ok(Json.toJson(Book.list))
@@ -25,24 +27,5 @@ object BooksController extends Controller {
       }
     )
   }
-
-
-  implicit val bookWrites: Writes[Book] = (
-    (JsPath \ "id").write[Int] and
-    (JsPath \ "image").write[String] and
-    (JsPath \ "title").write[String] and
-    (JsPath \ "author").write[String] and
-    (JsPath \ "price").write[Double] and
-    (JsPath \ "link").write[String]
-  )(unlift(Book.unapply))
-
-  implicit val bookReads: Reads[Book] = (
-    (JsPath \ "id").read[Int] and
-    (JsPath \ "image").read[String] and
-    (JsPath \ "title").read[String] and
-    (JsPath \ "author").read[String] and
-    (JsPath \ "price").read[Double] and
-    (JsPath \ "link").read[String]
-  )(Book.apply _)
 
 }
--- app/models/Book.scala
+++ app/models/Book.scala
@@ -1,17 +1,42 @@
 package models
 
+import play.api.libs.json.{Reads, JsPath, Writes}
+import play.api.libs.functional.syntax._
+
 case class Book(id: Int, image: String, title: String, author: String, price: Double, link: String)
 
 object Book {
+
   var list: List[Book] = {
-    List(
-      Book(1, "image1", "title1", "author1", 1.11, "link1"),
-      Book(2, "image2", "title2", "author2", 2.22, "link2")
-    )
+    1.until(99).map { n =>
+      Book(n, s"image$n", s"title$n", s"author$n", n, s"link$n")
+    }.toList
   }
 
   def save(book: Book) = {
     list = list ::: List(book)
   }
+
+}
+
+trait BookSerializer {
+
+  implicit val bookWrites: Writes[Book] = (
+    (JsPath \ "id").write[Int] and
+      (JsPath \ "image").write[String] and
+      (JsPath \ "title").write[String] and
+      (JsPath \ "author").write[String] and
+      (JsPath \ "price").write[Double] and
+      (JsPath \ "link").write[String]
+    )(unlift(Book.unapply))
+
+  implicit val bookReads: Reads[Book] = (
+    (JsPath \ "id").read[Int] and
+      (JsPath \ "image").read[String] and
+      (JsPath \ "title").read[String] and
+      (JsPath \ "author").read[String] and
+      (JsPath \ "price").read[Double] and
+      (JsPath \ "link").read[String]
+    )(Book.apply _)
 
 }
